f35196264634c9ca05cb8af91740c8736741fd95
Julian TOMCZYK <julian.tomczyk@laposte.fr>
2021-11-25 09:53:35 +0100

    Initial commit

---
+++ src/main/java/fr/mangashoten/dataLayer/DataLayerApplication.java
@@ -0,0 +1,13 @@
+package fr.mangashoten.dataLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/mangashoten/dataLayer/DataLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mangashoten.dataLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
