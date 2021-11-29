b38de904a7f62d4caff9293cd73170389d5569f5
Julian TOMCZYK <julian.tomczyk@laposte.fr>
2021-11-29 10:48:32 +0100

    Set up propertie file

--- src/main/java/fr/mangashoten/dataLayer/DataLayerApplication.java
+++ src/main/java/fr/mangashoten/dataLayer/DataLayerApplication.java
@@ -1,13 +1,29 @@
 package fr.mangashoten.dataLayer;
 
+import fr.mangashoten.dataLayer.model.User;
+import fr.mangashoten.dataLayer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.transaction.Transactional;
+
 @SpringBootApplication
-public class DataLayerApplication {
+public class DataLayerApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataLayerApplication.class, args);
 	}
 
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+
+		Iterable<User> users = userService.getUsers();
+		users.forEach(user -> System.out.println(user.getFirstName()));
+	}
 }
